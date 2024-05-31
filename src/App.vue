2f6850c19d90af3903b69fdfc1b712ac2e9cf1a7
Paula <paulafffl@gmail.com>
2024-05-31 18:51:13 +0100

    Create TennisBall component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,33 @@
   <h1>🎾 Reflex&nbsp;Game</h1>
   <h2>How fast are your reflexes? Find&nbsp;out&nbsp;now!</h2>
   <TennisCourt />
+  <button v-if="!hasPlayed" @click="start">PLAY</button>
+  <TennisBall v-if="isPlaying" :delay="delay" />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import TennisBall from "./components/TennisBall.vue";
 import TennisCourt from "./components/TennisCourt.vue";
 
 @Options({
   components: {
+    TennisBall,
     TennisCourt,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  isPlaying = false;
+  hasPlayed = false;
+  delay = 0;
+  start() {
+    this.delay = 2000 + Math.random() * 5000;
+    this.hasPlayed = true;
+    setTimeout(() => {
+      this.isPlaying = true;
+    }, this.delay);
+  }
+}
 </script>
 
 <style>
@@ -28,6 +43,23 @@ export default class App extends Vue {}
   @media (max-width: 767px) {
     justify-content: space-evenly;
   }
+}
+button {
+  background: #4d8dd7;
+  box-shadow: inset -0.3rem -0.3rem #084db2;
+  color: white;
+  border: none;
+  padding: 16px 22px;
+  padding-top: 14px;
+  border-radius: 14px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 40px auto;
+  font-size: 2rem;
+}
+button:hover {
+  background: #084db2;
+  transition: background-color 0.3s ease;
 }
 h1 {
   margin-bottom: 1rem;
---
+++ src/components/TennisBall.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>🥎</h1>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+};
+</script>
+
+<style scoped>
+h1 {
+  margin: 2rem;
+  cursor: pointer;
+}
+</style>
